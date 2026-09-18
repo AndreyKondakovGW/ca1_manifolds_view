94ab73a051165aedee616e2ec64bf9568d95c280
AndreyKondakovGW <andreykondakov2302@gmail.com>
2026-09-18 15:50:58 +0200

    update map change distance matrix colors scheme

--- app.js
+++ app.js
@@ -237,7 +237,7 @@ function renderGeodesic(session) {
     x: idx,
     y: idx,
     type: "heatmap",
-    colorscale: [[0, "blue"], [0.5, "white"], [1, "red"]],
+    colorscale: [[0, "#08519c"], [0.5, "white"], [1, "#d94801"]],
     colorbar: { title: "distance" },
     ...(customdata ? {
       customdata,
